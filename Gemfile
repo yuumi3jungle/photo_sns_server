60008b5402c3fd741918a79786682393a1acc7bc
Yuumi Yoshida <yy@ey-office.com>
2014-07-22 10:38:37 +0900

    Twitter bootstrap

--- Gemfile
+++ Gemfile
@@ -30,6 +30,7 @@ gem 'spring',        group: :development
 gem 'carrierwave'
 gem 'mini_magick'
 gem 'devise'
+gem 'bootstrap-sass'
 
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,2 +1,14 @@
 module PostsHelper
+  def popover_data_tag(title, content)
+    {class: "img-responsive", data: {toggle: "popover", placement: "auto", trigger: "hover", html: true,
+          title: title, content: simple_format(content)}}
+  end
+
+  def photo_title(timeline)
+    "#{timeline.user.name}さんが#{timeline.created_at.to_s(:short)}に撮影"
+  end
+
+  def full_url(path)
+    request.protocol + request.host_with_port + path
+  end
 end
