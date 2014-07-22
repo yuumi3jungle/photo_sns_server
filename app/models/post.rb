451a891db91ff7267f58f305d2693a11334f91fa
Yuumi Yoshida <yy@ey-office.com>
2014-07-22 11:54:08 +0900

    RSpec bugfixed

--- app/models/post.rb
+++ app/models/post.rb
@@ -8,7 +8,7 @@ class Post < ActiveRecord::Base
   validate :post_interval_time
 
   def post_interval_time
-    last_post = Post.last
+    last_post = Post.where(user_id: self.user_id).last
     if last_post && Time.now - last_post.created_at <= MINIMUM_POST_INTERVAL_SEC
       errors.add(:base, "#{MINIMUM_POST_INTERVAL_SEC}秒以内の連続投稿は出来ません")
     end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,19 +1,29 @@
 require 'rails_helper'
 
 RSpec.describe Post, :type => :model do
-  it "保存してから3秒以内は保存出来ない" do
-    post = Post.new(caption: 'test1', user_id:1)
-    post.save
-    post = Post.new(caption: 'test2', user_id:1)
-    expect(post.save).to be_falsey
+  context "同一ユーザーが" do
+    it "保存してから3秒以内は保存出来ない" do
+      post = Post.new(caption: 'test1', user_id:1)
+      post.save
+      post = Post.new(caption: 'test2', user_id:1)
+      expect(post.save).to be_falsey
+    end
+
+    it "保存してから3秒以後は保存出来る" do
+      post = Post.new(caption: 'test1', user_id:1)
+      post.save
+      allow(Time).to receive(:now) { Time.new + 4 }
+      post = Post.new(caption: 'test2', user_id:1)
+      expect(post.save).to be_truthy
+    end
   end
 
-  it "保存してから3秒以後は保存出来る" do
-    post = Post.new(caption: 'test1', user_id:1)
-    post.save
-
-    allow(Time).to receive(:now) { Time.new + 4 }
-    post = Post.new(caption: 'test2', user_id:1)
-    expect(post.save).to be_truthy
+  context "同一ユーザーでなければ" do
+    it "保存してから3秒以内でも保存出来る" do
+      post = Post.new(caption: 'test1', user_id:1)
+      post.save
+      post = Post.new(caption: 'test2', user_id:2)
+      expect(post.save).to be_truthy
+    end
   end
 end
